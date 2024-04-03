caa3374467ead0eae765f7d032342c47efd95c09
emilvdijk <emilvdijk@gmail.com>
2024-04-03 17:40:42 +0200

    commit 3

--- src/main/java/nl/emilvdijk/App.java
+++ src/main/java/nl/emilvdijk/App.java
@@ -9,68 +9,64 @@ import java.util.Scanner;
 
 public class App {
 
-  public static void main(String[] args){
-    String ch;
+    public static void main(String[] args) {
+        String ch;
 
-    HashMap<String, Integer> students = new HashMap<>();
-    Scanner myscanner = new Scanner(System.in);
+        HashMap<String, Integer> students = new HashMap<>();
+        Scanner myscanner = new Scanner(System.in);
 
-    students.put("Jantje", 2);
-    students.put("Eric", 13);
-    students.put("Evelien", 2346);
+        students.put("Jantje", 2);
+        students.put("Eric", 13);
+        students.put("Evelien", 2346);
 
-    do {
-      do {
-        System.out.println("""
-            Welkom in het studentendatabasesysteem.
-            1 om een persoon toe te voegen
-            2 om een persoon te verwijderen
-            3 om alle personen te tonen
-            'q' to quit""");
-        ch = myscanner.nextLine();
-      } while ((ch == "1") | (ch=="2") | (ch=="3") & (ch!="q"));
+        do {
+            do {
+                System.out.println("""
+                        Welkom in het studentendatabasesysteem.
+                        1 om een persoon toe te voegen
+                        2 om een persoon te verwijderen
+                        3 om alle personen te tonen
+                        'q' to quit""");
+                ch = myscanner.nextLine();
+            } while ((ch == "1") | (ch == "2") | (ch == "3") & (ch != "q"));
 
-      switch (ch) {
-        case "1":
-          System.out.println("Vul de naam van de nieuwe student in of \"q\" om te annuleren: \n");
-          String studentName = myscanner.nextLine();
-          if (Objects.equals(studentName, "q")) {
-            break;
-          }
-          System.out.println("Vul de leeftijd van de nieuwe student in: \n");
-          String studentAge = myscanner.nextLine();
-          students.put(studentName, Integer.valueOf(studentAge));
+            switch (ch) {
+                case "1":
 
-          break;
-        case "2":
-          System.out.println(
-              "Vul de naam van de te vervijderen student in of q om te annuleren: \n");
-          String studentDelete = myscanner.nextLine();
-          if (Objects.equals(studentDelete, "q")) {
-            break;
-          }
-          students.remove(studentDelete);
-          break;
-        case "3":
-          for (Entry<String, Integer> student : students.entrySet()) {
-            System.out.printf("naam: %-20s leeftijd: %d%n", student.getKey(), student.getValue());
-          }
-//          for (String i : students.keySet()) {
-//            System.out.print("naam: " + i + "\t\tleeftijd:\t");
-//            System.out.println(students.get(i));
-//      }
+                    System.out.println("Vul de naam van de nieuwe student in of \"q\" om te annuleren: \n");
+                    String studentName = myscanner.nextLine();
+                    if (Objects.equals(studentName, "q")) {
+                        break;
+                    }
+                    System.out.println("Vul de leeftijd van de nieuwe student in: \n");
 
-            /*
-printf in plaats van print geeft aan dat we een format willen gebruiken voor de variabelen in de string. Laten we %-20s ontleden: % houd in dat we op die plaats een variabel willen tonen, in dit geval de string i, de - geeft aan dat we de variabel links willen uitlijnen en de 20 geeft aan dat de variabel de ruimte van 20 characters moet in nemen, de s houdt in dat de variabel willen tonen als een string. %d houd in dat we de variabel, in dit geval students.get(i) willen tonen als een getal. %n voegt een nieuwe regel toe.
+                    while(!myscanner.hasNextInt()) {
+                          System.out.println("vul een cijfer in voor leeftijd!");
+                          myscanner.next();
+                    }
+                    int studentAge = myscanner.nextInt();
+                    students.put(studentName, studentAge);
+                    myscanner.skip("\n");
+                    break;
 
-Meer over de formatter kun je hier lezen: https://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax
-             */
+                case "2":
+                    System.out.println(
+                            "Vul de naam van de te vervijderen student in of q om te annuleren: \n");
+                    String studentDelete = myscanner.nextLine();
+                    if (Objects.equals(studentDelete, "q")) {
+                        break;
+                    }
+                    students.remove(studentDelete);
+                    break;
+                case "3":
+                    for (Entry<String, Integer> student : students.entrySet()) {
+                        System.out.printf("naam: %-20s leeftijd: %d%n", student.getKey(), student.getValue());
+                    }
+                    break;
+            }
 
-          break;
-      }
+            System.out.println();
 
-      System.out.println();
-
-    } while (!ch.equals("q"));
-  }
+        } while (!ch.equals("q"));
+    }
 }
